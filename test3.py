edb90f58757809d81e54f5be43b10dea9cda59c2
EwarystLawecki <e.lawecki@tkhtechnology.com>
2023-06-13 19:07:51 +0200

    aco

--- test3.py
+++ test3.py
@@ -1,110 +1,24 @@
 import random
-from networkx import Graph, draw
-import matplotlib.pyplot as plt
-import networkx as nx
-import os
-import re
 import time
-
-
-#STALE:
-N = 100
-K = 10
-
-# stworzenie nitki DNA
-def generate_dna_sequence(length):
-    nucleotides = ['A', 'T', 'C', 'G']
-    sequence = ''
-
-    for _ in range(length):
-        nucleotide = random.choice(nucleotides)
-        sequence += nucleotide
-
-    return sequence
-
-# Tworzymy zbiór oligonukleotydów
-def generate_subsequences(sequence, k):
-    subsequences = []
-    for i in range(len(sequence) - k + 1):
-        subsequence = sequence[i:i + k]
-        subsequences.append(subsequence)
-    return subsequences
-
-def save_sequence_to_file(sequence, folder, filename):
-    # Tworzenie ścieżki do folderu
-    folder_path = os.path.join(os.getcwd(), folder)
-
-    # Sprawdzanie istnienia folderu
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    # Tworzenie ścieżki do pliku
-    file_path = os.path.join(folder_path, filename)
-
-    with open(filename, 'w') as file:
-        file.write(sequence)
-
-
-def save_subsequences_to_file(subsequences, folder, filename):
-    # Tworzenie ścieżki do folderu
-    folder_path = os.path.join(os.getcwd(), folder)
-
-    # Sprawdzanie istnienia folderu
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    # Tworzenie ścieżki do pliku
-    file_path = os.path.join(folder_path, filename)
-
-    # Zapisywanie danych do pliku
-    with open(file_path, 'w') as file:
-        for subsequence in subsequences:
-            file.write(subsequence + '\n')
-
-
-def generate_and_save_sequences(N, K, folder):
-    for i in range(4):
-        # Generowanie nitki DNA
-        dna_sequence = generate_dna_sequence(N)
-
-        filename = f'dna_sequence_{i + 1}.txt'
-        save_sequence_to_file(dna_sequence, folder, filename)
-
-        # Generowanie sekwencji podnapisów
-        ordered_subsequences = generate_subsequences(dna_sequence, K)
-
-        # Zapisywanie sekwencji podnapisów do pliku
-        filename = f'sequences_{i + 1}.txt'
-        save_subsequences_to_file(ordered_subsequences, folder, filename)
-
-
-dna_sequence = generate_dna_sequence(N)
-ordered_subsequences = generate_subsequences(dna_sequence, K)
-shuffled_subsequences = random.sample(ordered_subsequences, len(ordered_subsequences))
-
-save_sequence_to_file(dna_sequence, 'input', 'dane.txt')
-save_subsequences_to_file(shuffled_subsequences, 'input', 'sequences.txt')
-
-
 
 def read_file(filename):
     nodes = []
     with open(filename) as f:
         lines = f.readlines()
-    for idx, i in enumerate(lines):
-        value = i.replace('\n', '')
-        nodes.append(Node(value, idx))
+    for i, line in enumerate(lines):
+        value = line.replace('\n', '')
+        nodes.append(Node(value, i))
 
     paths = [[Path(0, 1) for _ in range(len(nodes))] for _ in range(len(nodes))]
 
     return nodes, paths
 
 
-def set_lengths(nodes, paths):
+def set_weights(nodes, paths):
     for y, node_y in enumerate(nodes):
         for x, node_x in enumerate(nodes):
             if x != y:
-                value = node_y.set_distance(node_x)
+                value = node_y.get_distance(node_x)
                 paths[y][x].length = value
     return paths
 
@@ -163,8 +77,7 @@ class AntColony:
         for i in range(self.iteration):
             print("iteration:", i + 1)
             for ant in range(self.ants):
-                # print("ant:", ant + 1)
-                temp = Ant(n, nodes[random.randint(0, len(nodes) - 1)])
+                temp = Ant(n, nodes[random.randint(0, len(nodes) - 1)]) # mrówka startuje z losowego wierzchołka
                 temp.travel_road(paths, nodes)
             self.release_pheromones(paths)
 
@@ -179,12 +92,12 @@ class Node:
         self.value = value
         self.id = id
 
-    def set_distance(self, other):
+    def get_distance(self, other):
         for i in range(len(self.value)):
             if self.value[i:] == other.value[:len(other.value) - i]:
                 return i
         return len(self.value)
-
+    
     def get_path(self, other, paths):
         return paths[self.id][other.id]
 
@@ -201,7 +114,7 @@ class Path:
         self.pheromone = pheromone
 
     def evaporate_pheromone(self):
-        self.pheromone *= (1 / p)
+        self.pheromone *= (1 / rho)
 
     def add_pheromone(self, result):
         self.pheromone += result * Q
@@ -215,26 +128,26 @@ class Path:
 
 if __name__ == "__main__":
 
-    p = 0.7
+    rho = 0.7
     Q = 0.7
     alfa = 1
     beta = 5
-    ants_number = 30
-    iterations_number = 10
+    colony_size = 30
+    steps = 1
 
     best_path = []
     best_result = 0
 
-    n = 209
+    n = 100
     nodes, paths = read_file('input/sequences.txt')
 
-    set_lengths(nodes, paths)
+    set_weights(nodes, paths)
 
     start_time = time.time()
 
-    colony = AntColony(ants_number, iterations_number)
+    colony = AntColony(colony_size, steps)
     colony.launch(paths, nodes)
-
+    
     final_length = sum(node.get_path(best_path[i + 1], paths).length for i, node in enumerate(best_path[:-1]))
 
     end_time = time.time()
@@ -250,9 +163,9 @@ if __name__ == "__main__":
         f.write("Number of words:" + str(len(best_path)) + '\n')
         f.write("Execution time:" + str(execution_time) + "seconds" + '\n')
         f.write("Best road: " + str(best_path) + '\n')
-        f.write("p:" + str(p) + '\n')
+        f.write("rho:" + str(rho) + '\n')
         f.write("Q:" + str(Q) + '\n')
         f.write("alfa:" + str(alfa) + '\n')
         f.write("beta:" + str(beta) + '\n')
-        f.write("ants number:" + str(ants_number) + '\n')
-        f.write("iterations number:" + str(iterations_number) + '\n')
+        f.write("ants number:" + str(colony_size) + '\n')
+        f.write("iterations number:" + str(steps) + '\n')
